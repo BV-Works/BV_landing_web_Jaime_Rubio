b1f96cb4b7b74fe00469b78e66cdcd9c9ef2b3b4
BV-Works <maildinerodeinternet@gmail.com>
2026-03-02 20:20:47 +0100

    style header and nav, add close button and open close behaviour on small devices

--- js/script.js
+++ js/script.js
@@ -1,7 +1,21 @@
-console.log("qlqlq");
-
 const menuToggle = document.getElementById("menu-toggle");
+const menuToggleClose = document.getElementById("menu-toggle-close");
 const navbar = document.getElementById("navbar");
+const overlay = document.getElementById("nav-overlay");
 menuToggle.addEventListener("click", () => {
   navbar.classList.toggle("active");
+  overlay.classList.toggle("active");
+  menuToggleClose.classList.toggle("active");
+});
+menuToggleClose.addEventListener("click", () => {
+  navbar.classList.remove("active");
+  overlay.classList.remove("active");
+  menuToggleClose.classList.remove("active");
+});
+window.addEventListener("resize", () => {
+  if (window.innerWidth > 762) {
+    navbar.classList.remove("active");
+    overlay.classList.remove("active");
+    menuToggleClose.classList.remove("active");
+  }
 });
